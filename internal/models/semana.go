d15fb88cf70a74a0547f00d691be83efa94d63dc
FabriConde <fabricon20@gmail.com>
2024-10-18 14:05:09 +0200

    #12 Se crea el enumero DiaSemanay  la estructura "Semana".
    Creación del tipo enumero para DiaSemana que tenga los días de la semana. Para cada valor se devuelve su nombre en formato string.
    Se elige Struct para crear el modelo de "Semana" porque permite agrupar varios valores de diferentes tipos.
    Semana contiene:
    - diaPreparacion: Se le asigna el enumero DiaSemana.
    closes #12

---
+++ internal/models/semana.go
@@ -0,0 +1,17 @@
+package models
+
+type DiaSemana string
+
+const (
+	Lunes     DiaSemana = "Lunes"
+	Martes    DiaSemana = "Martes"
+	Miercoles DiaSemana = "Miercoles"
+	Jueves    DiaSemana = "Jueves"
+	Viernes   DiaSemana = "Viernes"
+	Sabado    DiaSemana = "Sabado"
+	Domingo   DiaSemana = "Domingo"
+)
+
+type Semana struct {
+	diaPreparacion DiaSemana
+}
