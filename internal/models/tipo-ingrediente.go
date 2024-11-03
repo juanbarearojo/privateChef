416c3f2d3c9b1708ea383194175bc9c79dd1d26d
FabriConde <fabricon20@gmail.com>
2024-11-03 13:22:14 +0100

    Se elimina 'UsoComun' del Tipo de ingrediente y se añade 'semi-perecedero'.
    Los alimetos pueden clasificarse en:
    - perecederos: Carnes, frutas, lácteos, pescados, vegetales, etc.
    - no perecederos: Atún enlatado, Azúcar, café, arroz, sal o legumbres en bolsa, verduras enlatadas, etc.
    - semi-perecederos: Granos secos, cereales, frutos secos, galletas, frituras, etc.
    Por tanto, se quita el uso común por semi-perecedero. closes #16

--- internal/models/tipo-ingrediente.go
+++ internal/models/tipo-ingrediente.go
@@ -3,7 +3,7 @@ package models
 type TipoIngrediente string
 
 const (
-	Perecedero   TipoIngrediente = "Perecedero"
-	NoPerecedero TipoIngrediente = "NoPerecedero"
-	UsoComun     TipoIngrediente = "UsoComun"
+	Perecedero      TipoIngrediente = "perecedero"
+	NoPerecedero    TipoIngrediente = "no perecedero"
+	SemmiPerecedero TipoIngrediente = "semi-perecedero"
 )
