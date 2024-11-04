eb1d847d0b1d11a6b7473c2c2a66b41fdfb7fcda
FabriConde <fabricon20@gmail.com>
2024-11-04 20:30:43 +0100

    Se añaden constructores a la estructura Inventario e Ingrediente #11 #9
    Se añade un constructor a las estructuras Inventario con su unico campo.
    Se añade constructor a la estructura Ingrediente con todos sus campos y con una validación para añadir la fecha de caducidad si es un ingrediente perecedero. closes #11 #9

--- internal/models/ingrediente.go
+++ internal/models/ingrediente.go
@@ -9,3 +9,18 @@ type Ingrediente struct {
 	cantidad        float32
 	fechaCaducidad  *time.Time
 }
+
+func NewIngrediente(id int, nombre string, tipoIngrediente TipoIngrediente, cantidad float32, fechaCaducidad *time.Time) Ingrediente {
+	ingrediente := Ingrediente{
+		id:              id,
+		nombre:          nombre,
+		tipoIngrediente: tipoIngrediente,
+		cantidad:        cantidad,
+	}
+
+	if tipoIngrediente == Perecedero {
+		ingrediente.fechaCaducidad = fechaCaducidad
+	}
+
+	return ingrediente
+}
