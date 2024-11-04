11c058702067c6236a1c8fe20fd25a01f498175c
FabriConde <fabricon20@gmail.com>
2024-11-04 20:30:17 +0100

    Se eliman codigo al no cerrar issue con el commit. #11 #9
    Se borran los constructores de las estructuras Inventario e Ingrediente al no cerrar las issue desde el comentario del commit. closes #11 #9

--- internal/models/ingrediente.go
+++ internal/models/ingrediente.go
@@ -9,18 +9,3 @@ type Ingrediente struct {
 	cantidad        float32
 	fechaCaducidad  *time.Time
 }
-
-func NewIngrediente(id int, nombre string, tipoIngrediente TipoIngrediente, cantidad float32, fechaCaducidad *time.Time) Ingrediente {
-	ingrediente := Ingrediente{
-		id:              id,
-		nombre:          nombre,
-		tipoIngrediente: tipoIngrediente,
-		cantidad:        cantidad,
-	}
-
-	if tipoIngrediente == Perecedero {
-		ingrediente.fechaCaducidad = fechaCaducidad
-	}
-
-	return ingrediente
-}
--- internal/models/inventario.go
+++ internal/models/inventario.go
@@ -3,9 +3,3 @@ package models
 type Inventario struct {
 	ingredientes []Ingrediente
 }
-
-func NewInventario(ingredientes []Ingrediente) Inventario {
-	return Inventario{
-		ingredientes: ingredientes,
-	}
-}
