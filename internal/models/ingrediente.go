6caa411092c31890e217c88faba5f95208c089d1
FabriConde <fabricon20@gmail.com>
2024-11-06 18:41:02 +0100

    Se elimina el id de Ingrediente #9
    Se elimana el id de Ingrediente ya que no es necesario. closes #9

--- internal/models/ingrediente.go
+++ internal/models/ingrediente.go
@@ -3,16 +3,14 @@ package models
 import "time"
 
 type Ingrediente struct {
-	id              int
 	nombre          string
 	tipoIngrediente TipoIngrediente
 	cantidad        float32
 	fechaCaducidad  *time.Time
 }
 
-func NewIngrediente(id int, nombre string, tipoIngrediente TipoIngrediente, cantidad float32, fechaCaducidad *time.Time) Ingrediente {
+func NewIngrediente(nombre string, tipoIngrediente TipoIngrediente, cantidad float32, fechaCaducidad *time.Time) Ingrediente {
 	ingrediente := Ingrediente{
-		id:              id,
 		nombre:          nombre,
 		tipoIngrediente: tipoIngrediente,
 		cantidad:        cantidad,
