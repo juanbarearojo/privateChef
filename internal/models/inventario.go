8614973b48f1ed64460f8151e2f93856f40d0b68
FabriConde <fabricon20@gmail.com>
2024-11-14 22:29:50 +0100

    Se añade GetIngredientes. #11
    Se añade GetIngredientes para que desde el constuctor de Ingrediente se compruebe si ya existe ese Ingrediente o no. closes #11

--- internal/models/inventario.go
+++ internal/models/inventario.go
@@ -4,8 +4,12 @@ type Inventario struct {
 	ingredientes map[string]Ingrediente
 }
 
-func NewInventario() Inventario {
-	return Inventario{
+func NewInventario() *Inventario {
+	return &Inventario{
 		ingredientes: make(map[string]Ingrediente),
 	}
+}
+
+func (i *Inventario) GetIngredientes() map[string]Ingrediente {
+	return i.ingredientes
 }
