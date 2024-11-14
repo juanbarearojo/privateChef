28a7454e8942b2fc5767212f8fc5bfe14095553e
FabriConde <fabricon20@gmail.com>
2024-11-14 22:33:19 +0100

    Validaciones de Ingrediente. #9
    - Se valida que el formato de la fecha sea de tipo DD/MM/YY sino da error.
    - Se valida que la cantidad sea un valor positivo sino da error.
    - Se valida que cuando se vaya a añadir un Ingrediente se compruebe que si el nombre del Producto existe en el Inventario de un error y solo deje guardar si el nombre del Producto del Ingrediente no existe en el Inventario.

--- internal/models/ingrediente.go
+++ internal/models/ingrediente.go
@@ -1,17 +1,38 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Ingrediente struct {
 	producto       Producto
 	cantidad       float32
-	fechaCaducidad *time.Time
+	fechaCaducidad string
 }
 
-func NewIngrediente(producto Producto, cantidad float32, fechaCaducidad *time.Time) Ingrediente {
-	return Ingrediente{
+func NewIngrediente(producto Producto, cantidad float32, fechaCaducidad string, inventario *Inventario) (*Ingrediente, error) {
+	if cantidad <= 0 {
+		return nil, errors.New("la cantidad debe ser mayor que 0")
+	}
+
+	fecha, err := time.Parse("02/01/2006", fechaCaducidad)
+	if err != nil {
+		return nil, errors.New("la fecha de caducidad debe tener el formato DD/MM/YYYY")
+	}
+
+	if fecha.Before(time.Now()) {
+		return nil, errors.New("la fecha de caducidad debe ser superior al año actual")
+	}
+
+	ingredientes := inventario.GetIngredientes()
+	if _, existe := ingredientes[producto.nombre]; existe {
+		return nil, errors.New("el ingrediente ya está en el inventario")
+	}
+
+	return &Ingrediente{
 		producto:       producto,
 		cantidad:       cantidad,
 		fechaCaducidad: fechaCaducidad,
-	}
+	}, nil
 }
