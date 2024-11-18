ddae439fba1ec76c4888540b6f9029f2adecf74b
FabriConde <fabricon20@gmail.com>
2024-11-18 14:33:57 +0100

    Se elimina el constructor de Inventario. closes #11

--- internal/models/inventario.go
+++ internal/models/inventario.go
@@ -4,12 +4,6 @@ type Inventario struct {
 	ingredientes map[string]Ingrediente
 }
 
-func NewInventario() *Inventario {
-	return &Inventario{
-		ingredientes: make(map[string]Ingrediente),
-	}
-}
-
 func (i *Inventario) GetIngredientes() map[string]Ingrediente {
 	return i.ingredientes
 }
