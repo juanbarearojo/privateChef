bc70f0c39cd5cba2e3ed61737416b25a0f47699f
barearojo <barearojojuan@gmail.com>
2024-12-08 15:47:47 +0100

    juanbarearojo#29 implementacion SumaPerecederos
    Habiendo seguido la metodología TDD se implementa la función SumaPerecederos
    que su objetivo ya había sido definido mediante los test. Esta función será
    usada más adelante para un cambio en la lógica de negocio.
    
    closes juanbarearojo#29

--- internal/models/receta.go
+++ internal/models/receta.go
@@ -8,3 +8,13 @@ type Receta struct {
 func (r *Receta) GetIngredientes() map[Producto]uint64 {
 	return r.ingredientes
 }
+
+func (r *Receta) SumaPerecederos() uint64 {
+	var suma uint64 = 0
+	for producto, cantidad := range r.ingredientes {
+		if producto.tipo == Perecedero {
+			suma += cantidad
+		}
+	}
+	return suma
+}
