ca17003e5d08871d24b40d104e3ec16e77c4dbf9
barearojo <barearojojuan@gmail.com>
2024-12-08 15:26:30 +0100

    juanbarearojo#28 cambios test receta para seguir DRY
    Se  cambia el test para que use la funcion setupTestEnviroment siguiendo
    los principios DRY
    
    closes juanbarearojo#28

--- internal/models/receta_test.go
+++ internal/models/receta_test.go
@@ -5,36 +5,14 @@ import (
 	"testing"
 )
 
-func testRecetaGetIngredientes(t *testing.T) {
-	// Crear productos de ejemplo
-	harina := Producto{
-		nombre:         "Harina",
-		tipo:           NoPerecedero,
-		fechaCaducidad: nil,
-	}
+func TestRecetaGetIngredientes(t *testing.T) {
+	harina, _, levadura, _, _, recetas := setupTestEnvironment()
 
-	azucar := Producto{
-		nombre:         "Azúcar",
-		tipo:           NoPerecedero,
-		fechaCaducidad: nil,
-	}
-
-	// Crear un mapa de ingredientes para la receta
 	ingredientesEsperados := map[Producto]uint64{
-		harina: 2,
-		azucar: 1,
+		*harina:   2,
+		*levadura: 1,
 	}
-
-	// Crear la receta con los ingredientes
-	receta := Receta{
-		titulo:       "Pastel de Harina",
-		ingredientes: ingredientesEsperados,
-	}
-
-	// Obtener los ingredientes usando el método GetIngredientes
-	ingredientesObtenidos := receta.GetIngredientes()
-
-	// Verificar que los ingredientes obtenidos son iguales a los esperados
+	ingredientesObtenidos := recetas[0].GetIngredientes()
 	if !reflect.DeepEqual(ingredientesObtenidos, ingredientesEsperados) {
 		t.Errorf("Los ingredientes obtenidos no coinciden con los esperados.\nEsperados: %v\nObtenidos: %v", ingredientesEsperados, ingredientesObtenidos)
 	}
