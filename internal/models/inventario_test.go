d0f10bf3c0191237653cb7d5af75fed867c6a6ec
barearojo <barearojojuan@gmail.com>
2024-12-08 15:45:43 +0100

    juanbarearojo#28 eliminación comentarios creando ruido

--- internal/models/inventario_test.go
+++ internal/models/inventario_test.go
@@ -17,10 +17,8 @@ func TestGetDesperdicioNoNegativo(t *testing.T) {
 func TestGetDesperdicioValorCorrecto(t *testing.T) {
 	_, _, _, _, inventario, _ := setupTestEnvironment()
 
-	// Perecederos: Levadura(2) + Huevos(6) = 8
 	desperdicioEsperado := uint64(8)
 	desperdicioActual := inventario.GetDesperdicio()
-
 	if desperdicioActual != desperdicioEsperado {
 		t.Errorf("GetDesperdicio devolvió %d, se esperaba %d", desperdicioActual, desperdicioEsperado)
 	}
@@ -30,7 +28,6 @@ func TestInventarioCloneNoVacio(t *testing.T) {
 	_, _, _, _, inventario, _ := setupTestEnvironment()
 
 	inventarioClonado := inventario.Clone()
-
 	if len(inventarioClonado.ingredientes) == 0 {
 		t.Errorf("El inventario clonado está vacío")
 	}
@@ -40,7 +37,6 @@ func TestInventarioCloneIgualAlOriginal(t *testing.T) {
 	_, _, _, _, inventario, _ := setupTestEnvironment()
 
 	inventarioClonado := inventario.Clone()
-
 	if !reflect.DeepEqual(inventario.ingredientes, inventarioClonado.ingredientes) {
 		t.Errorf("El inventario clonado no es igual al original.\nOriginal: %v\nClonado: %v", inventario.ingredientes, inventarioClonado.ingredientes)
 	}
@@ -49,10 +45,8 @@ func TestInventarioCloneIgualAlOriginal(t *testing.T) {
 func TestAplicarAsignacionNoNull(t *testing.T) {
 	_, _, _, _, inventario, recetas := setupTestEnvironment()
 
-	// Probaremos con las dos primeras recetas: Pan Casero y Bizcocho
 	recetasTest := []Receta{recetas[0], recetas[1]}
 	nuevoInventario := inventario.aplicarAsignacion(recetasTest)
-
 	if nuevoInventario == nil {
 		t.Errorf("aplicarAsignacion devolvió un inventario nulo")
 	}
@@ -61,10 +55,8 @@ func TestAplicarAsignacionNoNull(t *testing.T) {
 func TestAplicarAsignacionInventarioCorrecto(t *testing.T) {
 	harina, azucar, levadura, huevos, inventarioInicial, recetas := setupTestEnvironment()
 
-	// Aplicamos "Pan Casero" y "Bizcocho"
 	recetasTest := []Receta{recetas[0], recetas[1]}
 	inventarioFinal := inventarioInicial.Clone().aplicarAsignacion(recetasTest)
-
 	inventarioEsperado := Inventario{
 		ingredientes: map[Producto]uint64{
 			*harina:   0, // 5 - (2+3) = 0
