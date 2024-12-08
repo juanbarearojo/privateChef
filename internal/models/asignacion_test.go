f80fa3f1716e4405e5bffb7bb3837190ae69fd7d
barearojo <barearojojuan@gmail.com>
2024-12-08 16:43:01 +0100

    juanbarearojo#28 creación test sePuedePreparar
    Se crean test para la función se puedePreparar así como más test para validar
    la función realizarAsignación ya que esta supone la lógica de negocio
    del proyecto.
    
    closes juanbarearojo#28

--- internal/models/asignacion_test.go
+++ internal/models/asignacion_test.go
@@ -53,33 +53,73 @@ func setupTestEnvironment() (harina, azucar, levadura, huevos *Producto, inventa
 	return
 }
 
-func TestRealizarAsignacionListaNoVacia(t *testing.T) {
-	harina, azucar, levadura, _, inventario, recetas := setupTestEnvironment()
-	recetasTest := []Receta{recetas[0], recetas[1]}
+func TestSePuedePrepararPanCasero(t *testing.T) {
+	harina, _, levadura, _, inventario, recetas := setupTestEnvironment()
+	receta := recetas[0] // "Pan Casero"
 	inventario.ingredientes[*harina] = 2
-	inventario.ingredientes[*azucar] = 1
 	inventario.ingredientes[*levadura] = 1
-	recetasAsignables := realizarAsignacion(recetasTest, inventario)
 
+	puede := sePuedePreparar(receta, &inventario)
+	if !puede {
+		t.Errorf("Se esperaba que 'Pan Casero' se pudiera preparar, pero no")
+	}
+}
+
+func TestNoSePuedePrepararPanCasero(t *testing.T) {
+	harina, _, levadura, _, inventario, recetas := setupTestEnvironment()
+	receta := recetas[0] // "Pan Casero"
+	inventario.ingredientes[*harina] = 2
+	inventario.ingredientes[*levadura] = 0
+
+	puede := sePuedePreparar(receta, &inventario)
+	if puede {
+		t.Errorf("Se esperaba que 'Pan Casero' no se pudiera preparar, pero sí")
+	}
+}
+
+func TestRealizarAsignacionListaNoVacia(t *testing.T) {
+	_, _, _, _, inventario, recetas := setupTestEnvironment()
+	recetasAsignables := realizarAsignacion(recetas, inventario)
 	if len(recetasAsignables) == 0 {
 		t.Errorf("realizarAsignacion devolvió una lista vacía de recetas")
 	}
 }
 
+func TestRealizarAsignacionInventarioVacio(t *testing.T) {
+	_, _, _, _, inventario, recetas := setupTestEnvironment()
+	inventario.ingredientes = make(map[Producto]uint64)
+	recetasAsignables := realizarAsignacion(recetas, inventario)
+	if len(recetasAsignables) != 0 {
+		t.Errorf("Se esperaba una lista vacía de recetas asignables cuando el inventario está vacío, pero se obtuvo %d recetas", len(recetasAsignables))
+	}
+}
+
 func TestRealizarAsignacionCantidadCorrecta(t *testing.T) {
 	harina, azucar, levadura, _, inventario, recetas := setupTestEnvironment()
-	recetasTest := []Receta{recetas[0], recetas[1]}
 	inventario.ingredientes[*harina] = 2
 	inventario.ingredientes[*azucar] = 1
 	inventario.ingredientes[*levadura] = 1
-	recetasAsignables := realizarAsignacion(recetasTest, inventario)
+	recetasAsignables := realizarAsignacion(recetas, inventario)
 
 	if len(recetasAsignables) != 1 {
 		t.Errorf("Se esperaba 1 receta asignable, se obtuvieron %d", len(recetasAsignables))
 	}
 }
 
-func TestAsignacionComparativa(t *testing.T) {
+func TestRealizarAsignacionUnicaPosibilidad(t *testing.T) {
+	harina, azucar, levadura, _, inventario, recetas := setupTestEnvironment()
+	inventario.ingredientes[*harina] = 2
+	inventario.ingredientes[*azucar] = 1
+	inventario.ingredientes[*levadura] = 1
+	recetasAsignables := realizarAsignacion(recetas, inventario)
+
+	recetaAsignada := recetasAsignables[0]
+	if recetaAsignada.titulo != "Pan Casero" {
+		t.Errorf("Se esperaba que la receta asignada fuera 'Pan Casero', pero fue '%s'", recetaAsignada.titulo)
+	}
+}
+
+func TestRealizarAsignacionComparativa(t *testing.T) {
 	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
 
 	asignacionHumana := []Receta{
@@ -97,5 +137,61 @@ func TestAsignacionComparativa(t *testing.T) {
 
 	if desperdicioHumano <= desperdicioOptimo {
 		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
+	}
+}
+
+func TestRealizarAsignacionComparativa2(t *testing.T) {
+	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
+
+	asignacionHumana := []Receta{
+		recetas[1],
+		recetas[3],
+	}
+
+	inventarioHumano := inventarioInicial.Clone()
+	inventarioHumano = inventarioHumano.aplicarAsignacion(asignacionHumana)
+	desperdicioHumano := inventarioHumano.GetDesperdicio()
+	asignacionOptima := realizarAsignacion(recetas, inventarioInicial)
+	inventarioOptimo := inventarioInicial.Clone()
+	inventarioOptimo = inventarioOptimo.aplicarAsignacion(asignacionOptima)
+	desperdicioOptimo := inventarioOptimo.GetDesperdicio()
+
+	if desperdicioHumano <= desperdicioOptimo {
+		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
+	}
+}
+
+func TestRealizarAsignacionComparativa3(t *testing.T) {
+	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
+
+	asignacionHumana := []Receta{
+		recetas[2],
+		recetas[3],
+	}
+
+	inventarioHumano := inventarioInicial.Clone()
+	inventarioHumano = inventarioHumano.aplicarAsignacion(asignacionHumana)
+	desperdicioHumano := inventarioHumano.GetDesperdicio()
+	asignacionOptima := realizarAsignacion(recetas, inventarioInicial)
+	inventarioOptimo := inventarioInicial.Clone()
+	inventarioOptimo = inventarioOptimo.aplicarAsignacion(asignacionOptima)
+	desperdicioOptimo := inventarioOptimo.GetDesperdicio()
+
+	if desperdicioHumano <= desperdicioOptimo {
+		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
+	}
+}
+
+func TestRealizarAsignacionDesperdicioCero(t *testing.T) {
+	harina, azucar, levadura, huevos, inventario, recetas := setupTestEnvironment()
+	inventario.ingredientes[*harina] = 3
+	inventario.ingredientes[*azucar] = 1
+	inventario.ingredientes[*levadura] = 1
+	inventario.ingredientes[*huevos] = 1
+	recetasAsignables := realizarAsignacion(recetas, inventario)
+
+	desperdicio := inventario.Clone().aplicarAsignacion(recetasAsignables).GetDesperdicio()
+	if desperdicio != 0 {
+		t.Errorf("Se esperaba desperdicio cero, pero se obtuvo %d", desperdicio)
 	}
 }
