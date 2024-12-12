c51ea76dc483ea81fe470828568fd08ee9a3db81
barearojo <barearojojuan@gmail.com>
2024-12-12 10:22:52 +0100

    #28 Refactorizacion Siguiendo los principios DRY
    Había varias funciones que repetían codigo por eso se ha establecido una
    función nueva llamada ejecutarComparativaAsignacion

--- internal/models/asignacion_test.go
+++ internal/models/asignacion_test.go
@@ -96,12 +96,17 @@ func TestRealizarAsignacionInventarioVacio(t *testing.T) {
 
 func TestRealizarAsignacionCantidadCorrecta(t *testing.T) {
 	harina, azucar, levadura, _, inventario, recetas := setupTestEnvironment()
-	inventario.ingredientes[*harina] = 2
-	inventario.ingredientes[*azucar] = 1
-	inventario.ingredientes[*levadura] = 1
+
+	valorMinimoHacerPanHarina := uint64(2)
+	valorMinimoHacerPanAzucar := uint64(1)
+	valorMinimoHacerPanLevadura := uint64(1)
+	inventario.ingredientes[*harina] = valorMinimoHacerPanHarina
+	inventario.ingredientes[*azucar] = valorMinimoHacerPanAzucar
+	inventario.ingredientes[*levadura] = valorMinimoHacerPanLevadura
 	recetasAsignables := realizarAsignacion(recetas, inventario)
 
-	if len(recetasAsignables) != 1 {
+	numeroRecetasAsignablesEsperadas := 1
+	if len(recetasAsignables) != numeroRecetasAsignablesEsperadas {
 		t.Errorf("Se esperaba 1 receta asignable, se obtuvieron %d", len(recetasAsignables))
 	}
 }
@@ -119,17 +124,18 @@ func TestRealizarAsignacionUnicaPosibilidad(t *testing.T) {
 	}
 }
 
-func TestRealizarAsignacionComparativa(t *testing.T) {
+func ejecutarComparativaAsignacion(t *testing.T, indicesRecetas []int) {
 	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
 
-	asignacionHumana := []Receta{
-		recetas[0],
-		recetas[2],
+	asignacionHumana := make([]Receta, len(indicesRecetas))
+	for i, idx := range indicesRecetas {
+		asignacionHumana[i] = recetas[idx]
 	}
 
 	inventarioHumano := inventarioInicial.Clone()
 	inventarioHumano = inventarioHumano.aplicarAsignacion(asignacionHumana)
 	desperdicioHumano := inventarioHumano.GetDesperdicio()
+
 	asignacionOptima := realizarAsignacion(recetas, inventarioInicial)
 	inventarioOptimo := inventarioInicial.Clone()
 	inventarioOptimo = inventarioOptimo.aplicarAsignacion(asignacionOptima)
@@ -138,48 +144,21 @@ func TestRealizarAsignacionComparativa(t *testing.T) {
 	if desperdicioHumano <= desperdicioOptimo {
 		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
 	}
+}
+
+func TestRealizarAsignacionComparativa(t *testing.T) {
+	indicesRecetas := []int{0, 2}
+	ejecutarComparativaAsignacion(t, indicesRecetas)
 }
 
 func TestRealizarAsignacionComparativa2(t *testing.T) {
-	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
-
-	asignacionHumana := []Receta{
-		recetas[1],
-		recetas[3],
-	}
-
-	inventarioHumano := inventarioInicial.Clone()
-	inventarioHumano = inventarioHumano.aplicarAsignacion(asignacionHumana)
-	desperdicioHumano := inventarioHumano.GetDesperdicio()
-	asignacionOptima := realizarAsignacion(recetas, inventarioInicial)
-	inventarioOptimo := inventarioInicial.Clone()
-	inventarioOptimo = inventarioOptimo.aplicarAsignacion(asignacionOptima)
-	desperdicioOptimo := inventarioOptimo.GetDesperdicio()
-
-	if desperdicioHumano <= desperdicioOptimo {
-		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
-	}
+	indicesRecetas := []int{1, 3}
+	ejecutarComparativaAsignacion(t, indicesRecetas)
 }
 
 func TestRealizarAsignacionComparativa3(t *testing.T) {
-	_, _, _, _, inventarioInicial, recetas := setupTestEnvironment()
-
-	asignacionHumana := []Receta{
-		recetas[2],
-		recetas[3],
-	}
-
-	inventarioHumano := inventarioInicial.Clone()
-	inventarioHumano = inventarioHumano.aplicarAsignacion(asignacionHumana)
-	desperdicioHumano := inventarioHumano.GetDesperdicio()
-	asignacionOptima := realizarAsignacion(recetas, inventarioInicial)
-	inventarioOptimo := inventarioInicial.Clone()
-	inventarioOptimo = inventarioOptimo.aplicarAsignacion(asignacionOptima)
-	desperdicioOptimo := inventarioOptimo.GetDesperdicio()
-
-	if desperdicioHumano <= desperdicioOptimo {
-		t.Errorf("El desperdicio de la asignación humana (%d) no es mayor que el desperdicio óptimo (%d)", desperdicioHumano, desperdicioOptimo)
-	}
+	indicesRecetas := []int{2, 3}
+	ejecutarComparativaAsignacion(t, indicesRecetas)
 }
 
 func TestRealizarAsignacionDesperdicioCero(t *testing.T) {
