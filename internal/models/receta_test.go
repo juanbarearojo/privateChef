fac06905387268f412bab261d3462362c1683c36
barearojo <barearojojuan@gmail.com>
2024-12-13 12:04:53 +0100

    #28 Anaidido testSumaPerecederosHelper
    Se ha había detectado que en los de la suma de perecederos no se estaba siguiendo el princpio DRY. Debido a esto se crea una nueva función que evita que se repita el código desarrollado

--- internal/models/receta_test.go
+++ internal/models/receta_test.go
@@ -18,32 +18,31 @@ func TestRecetaGetIngredientes(t *testing.T) {
 	}
 }
 
-func TestRecetaSumaPerecederos(t *testing.T) {
+func testSumaPerecederosHelper(t *testing.T, recetaIndex int, sumaEsperada uint64) {
 	env := setupTestEnvironment()
+	receta := env.Recetas[recetaIndex]
+	sumaObtenida := receta.SumaPerecederos()
 
-	sumaEsperada := uint64(1)
-	sumaObtenida := env.Recetas[0].SumaPerecederos()
 	if sumaObtenida != sumaEsperada {
-		t.Errorf("La suma de perecederos es incorrecta.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
+		t.Errorf("La suma de perecederos para la receta '%s' es incorrecta.\nEsperada: %d\nObtenida: %d",
+			receta.titulo, sumaEsperada, sumaObtenida)
 	}
+}
+
+func TestRecetaSumaPerecederos(t *testing.T) {
+	recetaPan := 0
+	sumaEsperadaPan := uint64(1)
+	testSumaPerecederosHelper(t, recetaPan, sumaEsperadaPan)
 }
 
 func TestRecetaSumaPerecederosConMultiplesPerecederos(t *testing.T) {
-	env := setupTestEnvironment()
-
-	sumaEsperada := uint64(2)
-	sumaObtenida := env.Recetas[1].SumaPerecederos()
-	if sumaObtenida != sumaEsperada {
-		t.Errorf("La suma de perecederos es incorrecta.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
-	}
+	recetaBizcocho := 1
+	sumaEsperadaBizcocho := uint64(2)
+	testSumaPerecederosHelper(t, recetaBizcocho, sumaEsperadaBizcocho)
 }
 
 func TestRecetaSumaPerecederosSinPerecederos(t *testing.T) {
-	env := setupTestEnvironment()
-
-	sumaEsperada := uint64(0)
-	sumaObtenida := env.Recetas[3].SumaPerecederos()
-	if sumaObtenida != sumaEsperada {
-		t.Errorf("La suma de perecederos debe ser 0 cuando no hay perecederos.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
-	}
+	recetaPolvoDulce := 3
+	sumaEsperadaPolvo := uint64(0)
+	testSumaPerecederosHelper(t, recetaPolvoDulce, sumaEsperadaPolvo)
 }
