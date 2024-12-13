483f01269b024aac5f93711e19866b1c2eca9ddf
barearojo <barearojojuan@gmail.com>
2024-12-13 11:52:42 +0100

    #28 Actualización test recetas
    Debido a que la función setuptestEnvironment ha sido cambiada. Se actualizan
    los tets que el usaban para que reflejen este cambio. Se sabe que no es lo
    mejor el hecho de que las funciones tengan que llamar a una misma función
    pero testing solo deja establecer variables globales por lo si se hiciera
    eso sería necesario revisar el valor de las variables en cada test y por ende se terminaría repitiendo código que es lo que se está buscando evitar.

--- internal/models/receta_test.go
+++ internal/models/receta_test.go
@@ -6,41 +6,43 @@ import (
 )
 
 func TestRecetaGetIngredientes(t *testing.T) {
-	harina, _, levadura, _, _, recetas := setupTestEnvironment()
+	env := setupTestEnvironment()
 
 	ingredientesEsperados := map[Producto]uint64{
-		*harina:   2,
-		*levadura: 1,
+		*env.Harina:   2,
+		*env.Levadura: 1,
 	}
-	ingredientesObtenidos := recetas[0].GetIngredientes()
+	ingredientesObtenidos := env.Recetas[0].GetIngredientes()
 	if !reflect.DeepEqual(ingredientesObtenidos, ingredientesEsperados) {
 		t.Errorf("Los ingredientes obtenidos no coinciden con los esperados.\nEsperados: %v\nObtenidos: %v", ingredientesEsperados, ingredientesObtenidos)
 	}
 }
+
 func TestRecetaSumaPerecederos(t *testing.T) {
-	_, _, _, _, _, recetas := setupTestEnvironment()
+	env := setupTestEnvironment()
 
 	sumaEsperada := uint64(1)
-	sumaObtenida := recetas[0].SumaPerecederos()
+	sumaObtenida := env.Recetas[0].SumaPerecederos()
 	if sumaObtenida != sumaEsperada {
 		t.Errorf("La suma de perecederos es incorrecta.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
 	}
 }
 
 func TestRecetaSumaPerecederosConMultiplesPerecederos(t *testing.T) {
-	_, _, _, _, _, recetas := setupTestEnvironment()
+	env := setupTestEnvironment()
 
 	sumaEsperada := uint64(2)
-	sumaObtenida := recetas[1].SumaPerecederos()
+	sumaObtenida := env.Recetas[1].SumaPerecederos()
 	if sumaObtenida != sumaEsperada {
 		t.Errorf("La suma de perecederos es incorrecta.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
 	}
 }
 
 func TestRecetaSumaPerecederosSinPerecederos(t *testing.T) {
-	_, _, _, _, _, recetas := setupTestEnvironment()
+	env := setupTestEnvironment()
+
 	sumaEsperada := uint64(0)
-	sumaObtenida := recetas[3].SumaPerecederos()
+	sumaObtenida := env.Recetas[3].SumaPerecederos()
 	if sumaObtenida != sumaEsperada {
 		t.Errorf("La suma de perecederos debe ser 0 cuando no hay perecederos.\nEsperada: %d\nObtenida: %d", sumaEsperada, sumaObtenida)
 	}
